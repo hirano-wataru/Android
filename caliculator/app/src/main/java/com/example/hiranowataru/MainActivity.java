3fe207db34eda8ef925861e9aaf37af894718da2
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2018-10-23 17:59:28 +0900

    プロジェクトを作成

---
+++ caliculator/app/src/main/java/com/example/hiranowataru/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.hiranowataru;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = (TextView) findViewById(R.id.textview);
+        String test=textView.getText()+"20000";
+        textView.setText(test);
+
+    }
+}
